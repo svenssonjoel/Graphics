27f82048ba92b5eb342cc7987b478e78851759f5
Joel Svensson <svenssonjoel@yahoo.se>
2013-04-22 15:11:52 +0200

    update

--- SCANS/CUDA/sklansky3.cu
+++ SCANS/CUDA/sklansky3.cu
@@ -11,25 +11,24 @@
 
 
 /* ------------------------------------------------------------------------
-   Unrolled in-place(shared memory) Scan without syncs (32 threads, 64 elts) 
+   Unrolled in-place(shared memory) Scan without syncs (32 threads, 64 elts). 
+   Needs 2*64 elements of shared memory storage (512bytes).
+     (shared mem is 49152 bytes, but you share it with other blocks on an MP) 
    --------------------------------------------------------------------- */
 __device__ void skl_scan(int i, 
 			 float* input, 
 			 float *output, 
 			 float *s_data, // The shared memory
 			 float *maxs) { 
-  
-  // float *s_data = (float*)sbase;
-  
+    
   int tid = threadIdx.x; 
   int tids = tid << 1; 
 
   int eltOffs = blockIdx.x * 64 + tid; 
 
-  // Load data from  global memory into shared memory (in two separate load ops) 
+  // Load data from global memory into shared memory (in two separate load ops) 
   s_data[tid] = input[eltOffs]; 
-  s_data[tid + 32] = input[eltOffs + 32];
-    
+  s_data[tid + 32] = input[eltOffs + 32]; 
   // NO SYNC HERE 
 
   s_data[tids | 1] += s_data[tids]; 
@@ -50,7 +49,7 @@ __device__ void skl_scan(int i,
 
 
 /* ------------------------------------------------------------------------
-   The Scan kernel (Thousand(s?) of elements! NO SYNCS AT ALL) 
+   The Scan kernel (Thousand(s) of elements. NO SYNCS AT ALL) 
    --------------------------------------------------------------------- */
 __global__ void kernel(float* input0,
                        float* output0, 
@@ -60,18 +59,20 @@ __global__ void kernel(float* input0,
   extern __shared__ float s_data[]; 
   float *maxs = &s_data[64]; 
   
+  // Sequentially execute 64 scans
   for (int i = 0; i < 64; i ++) { 
     //sklansky(i,input0+i*32,output0+i*32,sbase,maxs);
     skl_scan(i,input0+i*64,output0+i*64,s_data,maxs);
   }
   
+  // in parallel scan the maximum array 
   float v; //discard this value.
   skl_scan(0,maxs,maxs,(float *)s_data,&v);
   
 
   // distribute (now in two phases) 
 
-  // 31 thread pass.
+  // 31 thread pass. 
   if (threadIdx.x > 0) {
     for (int j = 0; j < 64; j ++) {
       output0[(blockIdx.x*64)+(threadIdx.x*64+j)] += maxs[threadIdx.x-1];
