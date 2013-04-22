7ef281aaa497dbe26ce43c8a68cddd2f28cf1253
Joel Svensson <svenssonjoel@yahoo.se>
2013-04-22 14:29:15 +0200

    update

--- SCANS/CUDA/sklansky3.cu
+++ SCANS/CUDA/sklansky3.cu
@@ -7,16 +7,19 @@
 //#include "seqScan.c"
 
 
+#define N 64*64
+
+
 /* ------------------------------------------------------------------------
    Unrolled in-place(shared memory) Scan without syncs (32 threads, 64 elts) 
    --------------------------------------------------------------------- */
 __device__ void skl_scan(int i, 
 			 float* input, 
 			 float *output, 
-			 uint8_t *sbase, 
+			 float *s_data, // The shared memory
 			 float *maxs) { 
   
-  float *s_data = (float*)sbase;
+  // float *s_data = (float*)sbase;
   
   int tid = threadIdx.x; 
   int tids = tid << 1; 
@@ -45,34 +48,6 @@ __device__ void skl_scan(int i,
   
 }
 
-// Replace with handcoded scan that does not do conditionals.
-// This already exists in the scan work I did with Erik. 
-/*
-__device__ int sklansky(int i, float* input0, float *output0, uint8_t *sbase,float *maxs) {
-
-    uint32_t t2 = ((blockIdx.x*32)+((threadIdx.x&4294967294)|(threadIdx.x&1)));
-    uint32_t t9 = ((threadIdx.x&4294967292)|(threadIdx.x&3));
-    uint32_t t14 = ((threadIdx.x&4294967288)|(threadIdx.x&7));
-    uint32_t t19 = ((threadIdx.x&4294967280)|(threadIdx.x&15));
-    ((float*)sbase)[threadIdx.x] = (((threadIdx.x&1)<1) ? input0[t2] : (input0[((blockIdx.x*32)+((threadIdx.x&4294967294)|0))]+input0[t2]));
-    //__syncthreads();
-    ((float*)(sbase+128))[threadIdx.x] = (((threadIdx.x&3)<2) ? ((float*)sbase)[t9] : (((float*)sbase)[((threadIdx.x&4294967292)|1)]+((float*)sbase)[t9]));
-    //__syncthreads();
-    ((float*)sbase)[threadIdx.x] = (((threadIdx.x&7)<4) ? ((float*)(sbase+128))[t14] : (((float*)(sbase+128))[((threadIdx.x&4294967288)|3)]+((float*)(sbase+128))[t14]));
-    //__syncthreads();
-    ((float*)(sbase+128))[threadIdx.x] = (((threadIdx.x&15)<8) ? ((float*)sbase)[t19] : (((float*)sbase)[((threadIdx.x&4294967280)|7)]+((float*)sbase)[t19]));
-    //__syncthreads();
-    ((float*)sbase)[threadIdx.x] = ((threadIdx.x<16) ? ((float*)(sbase+128))[threadIdx.x] : (((float*)(sbase+128))[15]+((float*)(sbase+128))[threadIdx.x]));
-    //__syncthreads();
-    
-    output0[((blockIdx.x*32)+threadIdx.x)] = ((float*)sbase)[threadIdx.x];
- 
-    if (threadIdx.x == 0) 
-      maxs[i] = ((float*)sbase)[31];
-    
-    return 0;
-} 
-*/
 
 /* ------------------------------------------------------------------------
    The Scan kernel (Thousand(s?) of elements! NO SYNCS AT ALL) 
@@ -81,42 +56,42 @@ __global__ void kernel(float* input0,
                        float* output0, 
                        float* maxout){
    
-  extern __shared__ __attribute__ ((aligned(16))) uint8_t sbase[];
-  
-  //  float *maxs = (float*)(sbase+(sizeof(float)*64));
-  float *maxs = (float*)(sbase+(sizeof(float)*64));
+  // shared data. (two different kinds. warp local and across warps.) 
+  extern __shared__ float s_data[]; 
+  float *maxs = &s_data[64]; 
   
   for (int i = 0; i < 64; i ++) { 
     //sklansky(i,input0+i*32,output0+i*32,sbase,maxs);
-    skl_scan(i,input0+i*64,output0+i*64,sbase,maxs);
+    skl_scan(i,input0+i*64,output0+i*64,s_data,maxs);
   }
   
-  float v; //  discard this value
-  //sklansky(0,maxs,maxs,sbase,&v);
-  skl_scan(0,maxs,maxs,sbase,&v);
+  float v; //discard this value.
+  skl_scan(0,maxs,maxs,(float *)s_data,&v);
   
 
   // distribute (now in two phases) 
 
-  // 31 thread pass
+  // 31 thread pass.
   if (threadIdx.x > 0) {
     for (int j = 0; j < 64; j ++) {
       output0[(blockIdx.x*64)+(threadIdx.x*64+j)] += maxs[threadIdx.x-1];
     }
   }
 
-  // 32 thread pass 
+  // 32 thread pass. 
   for (int j = 0; j < 64; j ++) {
     output0[((blockIdx.x+32)*64)+(threadIdx.x*64+j)] += maxs[threadIdx.x+31];
   }
-  
+
+  // This is a debug step. 
   maxout[threadIdx.x] = maxs[threadIdx.x];
   maxout[threadIdx.x+32] = maxs[threadIdx.x+32];
 
 }
 
-#define N 64*64
-
+/* ------------------------------------------------------------------------
+   MAIN
+   --------------------------------------------------------------------- */
 int main(void) {
   
   float v[N]; 
@@ -172,34 +147,3 @@ int main(void) {
 }
 
 
-
-/* 
-__global__ void kernel(float* input0,
-                       float* output0, 
-                       float* maxout){
-   
-  extern __shared__ __attribute__ ((aligned(16))) uint8_t sbase[];
-  
-  float *maxs = (float*)(sbase+(sizeof(float)*64));
-  
-  for (int i = 0; i < 32; i ++) { 
-
-    sklansky(i,input0+i*32,output0+i*32,sbase,maxs);
-  }
-  
-  float v; //  discard this value
-  sklansky(0,maxs,maxs,sbase,&v);
-  
-
-  // distribute 
-  if (threadIdx.x > 0) {
-    for (int j = 0; j < 32; j ++) {
-      output0[(blockIdx.x*32)+(threadIdx.x*32+j)] += maxs[threadIdx.x-1];
-      
-    }
-  }
-  
-  maxout[threadIdx.x] = maxs[threadIdx.x];
-
-}
-*/
